cd1ab29e6b52136026b2a1f9709538fb8ceb77d5
Nikita Koselev <koselev@gmail.com>
2022-12-30 11:07:09 +0300

    Methods were implemented, but there are few bugs there.

--- logs_logs_logs.go
+++ logs_logs_logs.go
@@ -1,18 +1,32 @@
 package logs
 
+import "strings"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	if strings.Contains(log, "❗") {
+		return "recommendation"
+	}
+
+	if strings.Contains(log, "🔍") {
+		return "search"
+	}
+
+	if strings.Contains(log, "☀") {
+		return "weather"
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return len(log) <= limit
 }
